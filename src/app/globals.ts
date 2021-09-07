d58beaccb1fbfe06b5ee4b3441c540677ae7d235
pwiorek <piotr.wiorek71@gmail.com>
2021-09-07 14:01:47 +0200

    feat: setup resolution change triggers

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -5,3 +5,9 @@ export class Globals {
   BASE_URL = 'https://queapi.jupw.net/api/v1';
   VERSION = '1.0.0';
 }
+
+export enum ViewBreakpoints {
+  phone = '688px',
+  tablet = '992px',
+  laptop = '1280px',
+}
