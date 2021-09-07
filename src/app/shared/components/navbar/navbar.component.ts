686570271fc0a2248509758e85191419e038e5e7
pwiorek <piotr.wiorek71@gmail.com>
2021-09-07 14:29:22 +0200

    feat: create mobile layout for navbar component

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -2,6 +2,8 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { AuthenticationService } from 'src/app/auth/services/authentication.service';
+import {MediaService} from '../../services/media.service';
+import {ViewBreakpoints} from '../../../globals';
 
 
 export interface NavElement {
@@ -29,21 +31,22 @@ export enum ActiveNavElement {
 export class NavbarComponent implements OnInit {
   @Input('active') activeNavElement: ActiveNavElement;
 
-  list: NavElement[] = [
-    { icon: 'event', text: 'SCHEDULE', link: 'schedule', verifier: ActiveNavElement.SCHEDULE},
-    { icon: 'swap_horiz', text: 'EXCHANGE' + '\n' + 'REQUEST', link: 'exchanges', verifier: ActiveNavElement.EXCHANGE_SEND},
-    { icon: 'save_alt', text: 'INCOMING' + '\n' + 'REQUESTS', link: 'exchanges/incoming', verifier: ActiveNavElement.EXCHANGE_INCOMING},
-    { icon: 'publish', text: 'OUTGOING' + '\n' + 'REQUESTS', link: 'exchanges/outgoing', verifier: ActiveNavElement.EXCHANGE_OUTGOING},
-    { icon: 'accessibility_new', text: 'YOUR' + '\n' + 'QUEUE', link: 'queue', verifier: ActiveNavElement.QUEUE},
-    { icon: 'description', text: 'NOTES', link: 'https://zskpoznan.sharepoint.com/sites/Notatki2B/Shared%20Documents/Forms/AllItems.aspx', verifier: ActiveNavElement.NOTES}
-  ]
+  isMobile: boolean;
+  private mediaService = new MediaService(`(max-width: ${ViewBreakpoints.phone})`);
+
+  list: NavElement[] = [];
 
   constructor(
     private router: Router,
     private authenticationService: AuthenticationService,
   ) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.mediaService.match$.subscribe(value => {
+      this.isMobile = value;
+      this.mobileQuery();
+    });
+  }
 
   routeTo(location: string) {
     if (location.includes('https://')) window.open(location, '_blank');
@@ -52,6 +55,27 @@ export class NavbarComponent implements OnInit {
 
   logOut() {
     this.authenticationService.logOut();
+  }
+
+  mobileQuery(): void {
+    if (this.isMobile) {
+      this.list = [
+        { icon: 'event', text: 'SCHEDULE', link: 'schedule', verifier: ActiveNavElement.SCHEDULE},
+        { icon: 'swap_horiz', text: 'EXCHANGE' + '\n' + 'REQUEST', link: 'exchanges', verifier: ActiveNavElement.EXCHANGE_SEND},
+        { icon: 'save_alt', text: 'INCOMING' + '\n' + 'REQUESTS', link: 'exchanges/incoming', verifier: ActiveNavElement.EXCHANGE_INCOMING},
+        { icon: 'publish', text: 'OUTGOING' + '\n' + 'REQUESTS', link: 'exchanges/outgoing', verifier: ActiveNavElement.EXCHANGE_OUTGOING},
+        { icon: 'accessibility_new', text: 'YOUR' + '\n' + 'QUEUE', link: 'queue', verifier: ActiveNavElement.QUEUE},
+      ];
+    } else {
+      this.list = [
+        { icon: 'event', text: 'SCHEDULE', link: 'schedule', verifier: ActiveNavElement.SCHEDULE},
+        { icon: 'swap_horiz', text: 'EXCHANGE' + '\n' + 'REQUEST', link: 'exchanges', verifier: ActiveNavElement.EXCHANGE_SEND},
+        { icon: 'save_alt', text: 'INCOMING' + '\n' + 'REQUESTS', link: 'exchanges/incoming', verifier: ActiveNavElement.EXCHANGE_INCOMING},
+        { icon: 'publish', text: 'OUTGOING' + '\n' + 'REQUESTS', link: 'exchanges/outgoing', verifier: ActiveNavElement.EXCHANGE_OUTGOING},
+        { icon: 'accessibility_new', text: 'YOUR' + '\n' + 'QUEUE', link: 'queue', verifier: ActiveNavElement.QUEUE},
+        { icon: 'description', text: 'NOTES', link: 'https://zskpoznan.sharepoint.com/sites/Notatki2B/Shared%20Documents/Forms/AllItems.aspx', verifier: ActiveNavElement.NOTES}
+      ];
+    }
   }
 
 
